3b110734249923240ac24713013a00ffbb213e2c
Anastasia <anastasia7066084@mail.ru>
2023-04-04 17:31:48 +0300

    added DZ3

---
+++ 1.js
@@ -0,0 +1,2 @@
+let celsius =  Number.parseFloat(prompt("Введите градусы по Цельсию "));
+alert(`Температура по Фаренгейту равна: ${((9 / 5) * celsius + 32).toFixed(1)}`);
---
+++ 2.js
@@ -0,0 +1,5 @@
+let name1 = prompt("Введите имя: ");
+function greeting(username) {
+    console.log(`Привет,${name1}`);
+}
+greeting(name1);
